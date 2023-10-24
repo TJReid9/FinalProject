1b612a28f97aa717b25f1ff2111f6f528e9d9bc8
Reid Snyder <reidsnyder7@gmail.com>
2023-10-24 14:07:47 -0600

    Add bootstrap/ng-bootstrap to angular dependencies

--- ngWatchParty/src/app/app.module.ts
+++ ngWatchParty/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
