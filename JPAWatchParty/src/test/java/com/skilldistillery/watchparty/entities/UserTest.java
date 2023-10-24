1796bedf4cfacd6c2dec3763cec740583806ec44
TJReid9 <tjreid20@icloud.com>
2023-10-24 10:39:03 -0600

    Stub JPA and Spring Boot projects

---
+++ JPAWatchParty/src/test/java/com/skilldistillery/watchparty/entities/UserTest.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.watchparty.entities;
+
+public class UserTest {
+
+}
---
+++ WatchParty/src/test/java/com/skilldistillery/watchparty/WatchPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.watchparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
