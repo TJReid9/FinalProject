e1e7273d7f5d4a37ea934ac2bb603d15f2d73190
Reid Snyder <reidsnyder7@gmail.com>
2023-10-26 09:23:25 -0600

    able to login and look at homepage in angular

--- ngWatchParty/src/app/app-routing.module.ts
+++ ngWatchParty/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UsersComponent } from './components/users/users.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  // { path: 'home', component: HomeComponent },
+   { path: 'home', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   // { path: 'users/:usersId', component: TodoListComponent },
   // { path: 'venues', component: AboutComponent },
