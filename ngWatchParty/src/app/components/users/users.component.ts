9db71ac5d0768c18cee04deb266c93a9d96b7599
Chiaya T <709210chiaya@gmail.com>
2023-10-26 15:47:22 -0600

    able to connect user to loggedin user

--- ngWatchParty/src/app/components/users/users.component.ts
+++ ngWatchParty/src/app/components/users/users.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +11,52 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+loggedInUser: User = new User;
+selectedUser: User | null = null;
 
+constructor(
+  private userService: UserService,
+  private activatedRoute: ActivatedRoute,
+  private router: Router,
+  private auth: AuthService
+){}
+
+ngOnInit(): void{
+  this.setLoggedInUser();
+  this.activatedRoute.paramMap.subscribe({
+    next: (params) => {
+      let userIdStr = params.get('userId');
+      if (userIdStr){
+        let userId = parseInt(userIdStr);
+        if (isNaN(userId)){
+          this.router.navigateByUrl("/invalidUser");
+        }else{
+          this.userService.show(userId).subscribe({
+            next: (user) => {
+              this.selectedUser = user;
+            },
+            error: (err) => {
+              console.log('UserListHttpComponent.show(), error getting user');
+              this.router.navigateByUrl("/invalidUser");
+            }
+
+          })
+        }
+      }
+    }
+  })
+}
+
+setLoggedInUser(){
+  this.auth.getLoggedInUser().subscribe({
+    next: (user) => {
+      this.loggedInUser = user;
+      console.log(user);
+    },
+    error: (err) => {
+      console.error('UserComponent.setLoggedInUser: error getting logged in user')
+    }
+  });
+}
 
 }
