e1831c5ee247ddde534ae0eaa89e43d57ae2fba6
Reid Snyder <reidsnyder7@gmail.com>
2023-10-26 11:11:11 -0600

    made changes in navbar so we can login there

--- ngWatchParty/src/app/components/navigation/navigation.component.ts
+++ ngWatchParty/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -10,11 +11,19 @@ export class NavigationComponent {
   public isCollapsed = false;
 
   constructor(
-   private auth: AuthService,
+   private auth: AuthService, private router: Router
   ){}
 
   loggedIn(): boolean {
     return this.auth.checkLogin();
   }
 
+
+  goToLogin() {
+    this.router.navigateByUrl('/login')
+  }
+
+  goToRegister() {
+    this.router.navigateByUrl('/register')
+  }
 }
