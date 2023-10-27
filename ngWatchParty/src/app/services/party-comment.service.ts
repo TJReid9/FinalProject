8ab3ba60a10710060b47ededcc9cace10c194ac9
TJReid9 <tjreid20@icloud.com>
2023-10-27 09:56:31 -0600

    Created services for: venue_rating, party_rating, user_comment, party_comment

---
+++ ngWatchParty/src/app/services/party-comment.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { AuthService } from './auth.service';
+import { Observable, catchError, throwError } from 'rxjs';
+import { PartyComment } from '../models/party-comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PartyCommentService {
+
+  private url = environment.baseUrl + 'api/';
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<PartyComment[]> {
+    return this.http.get<PartyComment[]>(this.url + 'watchparties/partyComments').pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('PartCommentService.index(): error retrieving List of Party Comments: ' + err)
+        );
+      })
+    );
+  }
+
+  show(partyCommentId: number): Observable<PartyComment> {
+    return this.http.get<PartyComment>(this.url + 'watchparties/partyComments/'+ partyCommentId, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('PartCommentService.show(): error retrieving Party Comments: ' + err)
+        );
+      })
+    );
+  }
+
+  create(partyComment: PartyComment): Observable<PartyComment> {
+    console.log(partyComment)
+    return this.http.post<PartyComment>(this.url + 'watchparties/partyComments/', partyComment, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'PartCommentService.create(): error creating Party Comments: ' + err )
+        );
+      })
+    );
+  }
+
+  update(partyCommentId: number, partyRating: PartyComment): Observable<PartyComment> {
+    return this.http.put<PartyComment>(this.url + 'watchparties/partyRatings/' + partyCommentId, partyRating ,this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'PartyRatingService.update(): error updating Party Rating: ' + err )
+        );
+      })
+    );
+  }
+
+  destroy(partyCommentId: number) : Observable<void> {
+    return this.http.delete<void>(this.url + 'watchparties/partyRatings/'+ partyCommentId, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'PartyRatingService.destroy(): error deleting Party Rating: ' + err )
+        );
+      })
+    );
+  }
+}
---
+++ ngWatchParty/src/app/services/user-comment.service.ts
@@ -0,0 +1,85 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../models/user';
+import { AuthService } from './auth.service';
+import { UserComment } from '../models/user-comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserCommentService {
+
+  private url = environment.baseUrl + 'api/';
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<UserComment[]> {
+    return this.http.get<UserComment[]>(this.url + 'watchparties/userComments').pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('PartyService.index(): error retrieving List of Comments: ' + err)
+        );
+      })
+    );
+  }
+
+  show(userCommentId: number): Observable<UserComment> {
+    return this.http.get<UserComment>(this.url + 'watchparties/userComments/'+ userCommentId).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('UserCommentService.show(): error retrieving User Comment: ' + err)
+        );
+      })
+    );
+  }
+
+  create(userComment: UserComment): Observable<UserComment> {
+    console.log(userComment)
+    return this.http.post<UserComment>(this.url + 'watchparties/userComments/', userComment).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'UserCommentService.create(): error creating User Comments: ' + err )
+        );
+      })
+    );
+  }
+
+  update(userCommentId: number, userComment: UserComment): Observable<UserComment> {
+    return this.http.put<UserComment>(this.url + 'watchparties/userComments/' + userCommentId, userComment).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'UserCommentService.update(): error updating User Comment: ' + err )
+        );
+      })
+    );
+  }
+
+  destroy(userCommentId: number) : Observable<void> {
+    return this.http.delete<void>(this.url + 'watchparties/userComments/'+ userCommentId).pipe(
+      catchError((err: any) => {
+        console.error(err);
+        return throwError(
+           () => new Error( 'UserCommentService.destroy(): error deleting User Comment: ' + err )
+        );
+      })
+    );
+  }
+}
